c592a8f9e581e020f16ad41990836260366b0c4f
Anxheloo <anxhelocenollari@gmail.com>
2024-07-23 15:40:16 +0200

    EventProps

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import Greet from "./components/Greet";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Person from "./components/Person";
-import PersonList from "./components/PersonList";
-import Status from "./components/Status";
-
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
-  const personList = [
-    { first: "Bruce", last: "Wayne" },
-    { first: "Darli", last: "Varfi" },
-    { first: "Karl", last: "Larfi" },
-  ];
-
-  return (
-    <div>
-      <Greet name="Anxhelo" messageCount={20} isLoggedIn={true} />
-      <Person name={personName}></Person>
-      <PersonList list={personList}></PersonList>
-      <Status message="loading"></Status>
-      <Heading>Placeholder text</Heading>
-
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-
-      <Greet name="Beqo" isLoggedIn={true}></Greet>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
