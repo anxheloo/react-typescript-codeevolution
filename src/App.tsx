32fa5d4e6b9c88d4ed006ecc6234810189710981
Anxheloo <anxhelocenollari@gmail.com>
2024-07-23 15:30:25 +0200

    PassingArrayOfObjectsAsProp

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import Greet from "./components/Greet";
-import Heading from "./components/Heading";
-import Oscar from "./components/Oscar";
-import Person from "./components/Person";
 import PersonList from "./components/PersonList";
-import Status from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
-
   const personList = [
     { first: "Bruce", last: "Wayne" },
     { first: "Darli", last: "Varfi" },
@@ -19,17 +9,7 @@ function App() {
 
   return (
     <div>
-      <Greet name="Anxhelo" messageCount={20} isLoggedIn={true} />
-      <Person name={personName}></Person>
       <PersonList list={personList}></PersonList>
-      <Status message="loading"></Status>
-      <Heading>Placeholder text</Heading>
-
-      <Oscar>
-        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
-      </Oscar>
-
-      <Greet name="Beqo" isLoggedIn={true}></Greet>
     </div>
   );
 }
